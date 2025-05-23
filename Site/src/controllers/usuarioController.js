df7e9cce1549d187235a8ca3971c09bef2c9e64c
Leandro Apolinário Leite <97578213+Leandroapolinario@users.noreply.github.com>
2025-05-23 17:27:40 -0300

    conexções com a API corrirgidas e funcionando 100%(menos dashboard)

--- Site/src/controllers/usuarioController.js
+++ Site/src/controllers/usuarioController.js
@@ -3,11 +3,33 @@
 var usuarioModel = require("../models/usuarioModel");
 
 function cadastrar(req, res) {
-    // ... (seu código de cadastro)
+
+    var nomeUsuario = req.body.nomeServer;
+    var senha = req.body.senhaServer;
+
+    if (nomeUsuario === undefined || senha === undefined) {
+        res.status(400).send("Nome de usuário e senha são obrigatórios!");
+    }
+
+    usuarioModel.cadastrar(nomeUsuario, senha)
+    .then(
+        function (resultado) {
+            res.json(resultado)
+        }
+    ).catch(
+        function (erro) {
+            console.log(erro)
+            console.log(
+                "\nHouve um erro ao realizar cadastro",
+                erro.sqlMessage
+            )
+            res.status(500).json(erro.sqlMessage)
+        }
+    )
 }
 
 function logar(req, res) {
-    var nomeUsuario = req.body.emailServer; // O front-end ainda envia como emailServer, vamos usar esse valor como nome
+    var nomeUsuario = req.body.usuarioServer; // O front-end ainda envia como emailServer, vamos usar esse valor como nome
     var senha = req.body.senhaServer;
 
     if (nomeUsuario === undefined || senha === undefined) {
@@ -15,7 +37,7 @@ function logar(req, res) {
         return false;
     }
 
-    usuarioModel.logar(nomeUsuario)
+    usuarioModel.logar(nomeUsuario, senha)
         .then(
             function (resultado) {
                 if (resultado.length > 0) {
--- Site/src/models/usuarioModel.js
+++ Site/src/models/usuarioModel.js
@@ -3,16 +3,22 @@
 var database = require("../database/config");
 
 function cadastrar(nome, senha) {
+    console.log("ACESSEI O USUARIO MODEL PARA CADASTRO:", nome, senha);
+    var instrucaoSql = `
+         INSERT INTO usuario (nome, senha) VALUES ('${nome}', '${senha}')`;
     // ... (seu código de cadastro)
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
 }
 
-function logar(nomeUsuario) { // Alteramos o parâmetro para nomeUsuario
+
+function logar(nomeUsuario,senha) { // Alteramos o parâmetro para nomeUsuario
     console.log("ACESSEI O USUARIO MODEL PARA LOGIN COM NOME: ", nomeUsuario);
 
     var instrucaoSql = `
         SELECT idUsuario, nome, senha
         FROM usuario
-        WHERE nome = '${nomeUsuario}';
+        WHERE nome = '${nomeUsuario}' and senha = '${senha}';
     `;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
