8be7df8b71c7d228a9030a41d52cf1430b6a225e
Leandro Apolinário Leite <97578213+Leandroapolinario@users.noreply.github.com>
2025-05-29 14:20:07 -0300

    Banco de dados atualizado

--- BD/projeto-individual.sql
+++ BD/projeto-individual.sql
@@ -12,15 +12,29 @@ CREATE TABLE Usuario (
 
 CREATE TABLE Quiz (
     idQuiz INT PRIMARY KEY,
-    acerto INT,
-    erro INT
+    nome varchar (45)
 );
 
 CREATE TABLE pontuacao (
+	idPontuacao INT,
     pkUsuario INT,
     pkQuiz INT,
     pontos INT,
-    PRIMARY KEY (pkUsuario, pkQuiz),
+    PRIMARY KEY (pkUsuario, pkQuiz, idPontuacao),
     FOREIGN KEY (pkUsuario) REFERENCES Usuario(idUsuario),
     FOREIGN KEY (pkQuiz) REFERENCES Quiz(idQuiz)
 );
+
+CREATE TABLE pergunta (
+    idPergunta INT PRIMARY KEY,
+    pkQuiz INT,
+    questao varchar (45),
+    FOREIGN KEY (pkQuiz) REFERENCES Quiz(idQuiz)
+ );
+ 
+ CREATE TABLE alternativa (
+    idAlternativa INT PRIMARY KEY,
+    pkPergunta INT,
+    opcao varchar (45),
+    FOREIGN KEY (pkPergunta) REFERENCES Pergunta(idPergunta)
+ );
