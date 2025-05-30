a30672ec80c77d071e7758021d2eec2f6b223282
Leandro Apolinário Leite <97578213+Leandroapolinario@users.noreply.github.com>
2025-05-30 16:53:55 -0300

    ALterei o erro do quiz.Controller

--- Site/src/controllers/quizController.js
+++ Site/src/controllers/quizController.js
@@ -2,20 +2,7 @@ var quizModel = require("../models/quizModel");
 
 function buscarPerguntas(req, res) {
     quizModel.buscarPerguntas()
-        .then(resultado => res.json(resultado))
-        .catch(erro => {
-            console.error("Erro ao buscar perguntas:", erro)
-            res.status(500).json({ erro: "Erro ao buscar perguntas" })
-        });
-}
-
-function acertos(req, res) {
-
-    var pontuacao = req.body.pontuacaoServer
-    var idUsuario = req.body.idUsuarioServer
-
-    quizModel.acertos(idUsuario, pontuacao)
-        .then(resultado => res.json(resultado))
+        .then(resultado => res.json(resultado)) 
         .catch(erro => {
             console.error("Erro ao buscar perguntas:", erro)
             res.status(500).json({ erro: "Erro ao buscar perguntas" })
@@ -23,6 +10,5 @@ function acertos(req, res) {
 }
 
 module.exports = {
-    buscarPerguntas,
-    acertos
+    buscarPerguntas
 }
