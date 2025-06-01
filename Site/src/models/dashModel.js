46acf04463a777489afc9d1d40601f4a1617d96b
Leandro Apolinário Leite <97578213+Leandroapolinario@users.noreply.github.com>
2025-05-31 21:50:17 -0300

    pontuação do ranking atualizada

--- Site/src/models/dashModel.js
+++ Site/src/models/dashModel.js
@@ -1,13 +1,25 @@
-// usuarioModel.js
+// dashModel.js
 
 var database = require("../database/config");
 
 function buscar(idUsuario) {
-    console.log("ACESSEI A DASH MODEL PARA BUSCAR PONTUAÇÃO:", idUsuario);
+    console.log("ACESSEI A DASH MODEL PARA BUSCAR PONTUAÇÃO (TOP 10):", idUsuario);
     var instrucaoSql = `
-   select SUM(p.pontos) as pontos, u.nome as nome FROM pontuacao AS p
-    JOIN Quiz AS q ON q.idQuiz = p.fkQuiz
-    JOIN Usuario AS u ON u.idUsuario = p.fkUsuario group by u.nome;`;
+    SELECT
+        SUM(p.pontos) AS pontos,
+        u.nome AS nome
+    FROM
+        pontuacao AS p
+    JOIN
+        Quiz AS q ON q.idQuiz = p.fkQuiz
+    JOIN
+        Usuario AS u ON u.idUsuario = p.fkUsuario
+    GROUP BY
+        u.nome
+    ORDER BY
+        pontos DESC  -- Ordena os resultados pela pontuação de forma descendente
+    LIMIT 10;        -- Limita a consulta aos 10 primeiros resultados
+    `;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
