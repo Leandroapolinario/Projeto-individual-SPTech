8a18fc4955ada1581fa2e97e2c8fd93614309b38
Leandro Apolinário Leite <97578213+Leandroapolinario@users.noreply.github.com>
2025-05-31 21:15:08 -0300

    dashboard atualizada para a pontuação do mesmo usuário ir se somando

--- Site/src/models/dashModel.js
+++ Site/src/models/dashModel.js
@@ -5,10 +5,9 @@ var database = require("../database/config");
 function buscar(idUsuario) {
     console.log("ACESSEI A DASH MODEL PARA BUSCAR PONTUAÇÃO:", idUsuario);
     var instrucaoSql = `
-    select p.pontos, u.nome FROM pontuacao AS p
+   select SUM(p.pontos) as pontos, u.nome as nome FROM pontuacao AS p
     JOIN Quiz AS q ON q.idQuiz = p.fkQuiz
-    JOIN Usuario AS u ON u.idUsuario = p.fkUsuario;     
-    `;
+    JOIN Usuario AS u ON u.idUsuario = p.fkUsuario group by u.nome;`;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
